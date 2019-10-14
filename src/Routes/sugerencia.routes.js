03c5abd89a6ffbd55ea7833262f04e08d698db8c
Rogger <rogger.140998@gmail.com>
2019-10-14 15:07:29 -0500

    added: Not null allowed

--- src/Routes/sugerencia.routes.js
+++ src/Routes/sugerencia.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import { crearSugerencia } from '../controller/sugerencia.controller';
+import { crearSugerencia, editarSugerencia, listarSugerencias } from '../controller/sugerencia.controller';
 
 const router = Router();
 
+router.get('/', listarSugerencias)
 router.post('/', crearSugerencia);
+router.put('/:id', editarSugerencia);
 
 export default router;
--- src/controller/sugerencia.controller.js
+++ src/controller/sugerencia.controller.js
@@ -1,5 +1,28 @@
 import models from '../../sequelize/models';
 
+
+export function listarSugerencias(req, res) {
+    models.Sugerencia.findAll({
+            include: [{ // Notice `include` takes an ARRAY
+                model: models.Usuario,
+                attributes: ['nombre_usuario', 'nro_doc_person']
+            }]
+        })
+        .then(sugerencias => {
+            return res.status(200).json({
+                message: `Lista de Sugerencias`,
+                data: sugerencias
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                message: `Error al listar sugerencias`,
+                data: {}
+            })
+        })
+}
+
 export function crearSugerencia(req, res) {
     const { id_usuario, texto_sugerencia } = req.body;
     console.log("AGREGAR SUGERENCIA");
@@ -17,6 +40,33 @@ export function crearSugerencia(req, res) {
             console.log(error);
             return res.status(500).json({
                 message: "Error al crear la sugerencia",
+                data: {}
+            });
+        })
+
+}
+
+export function editarSugerencia(req, res) {
+    const { id } = req.params;
+    const { id_usuario, texto_sugerencia } = req.body;
+    console.log("EDITAR SUGERENCIA");
+    models.Sugerencia.update({
+            id_usuario,
+            texto_sugerencia
+        }, {
+            returning: true,
+            where: { id }
+        })
+        .then(([rowsUpdate, [sugerenciaUpdate]]) => {
+            return res.status(200).json({
+                message: `La sugerencia fue editada.`,
+                data: sugerenciaUpdate
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                message: "Error al crear o modificar la sugerencia",
                 data: {}
             });
         })
